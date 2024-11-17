7d6880ef1f849b7a6811a367d3a13267211fca9c
raobhavya01 <yadavbhavya102@gmail.com>
2024-11-17 22:40:13 +0530

    First Commit

---
+++ test-env.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+console.log('GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
+console.log('GOOGLE_CLIENT_SECRET:', process.env.GOOGLE_CLIENT_SECRET);
+console.log('All environment variables:', process.env);
